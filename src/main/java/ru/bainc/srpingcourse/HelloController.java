2001fd2fac5c658f69848a7e049b1b6dfdc626f1
Babushkina Olga <ba80@mail.ru>
2022-06-28 16:14:52 +0300

    Initial commit

---
+++ src/main/java/ru/bainc/srpingcourse/HelloController.java
@@ -0,0 +1,13 @@
+package ru.bainc.srpingcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
